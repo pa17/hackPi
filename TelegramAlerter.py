2e2a0001c2ce5b942dfde65f57722a2f38344f1c
Paolo Rüegg <paolo.rueegg@me.com>
2019-01-06 14:52:34 +0000

    Revised Adaptive Uploader and added TelegramAlerter: Scope Error

--- TelegramAlerter.py
+++ TelegramAlerter.py
@@ -46,6 +46,7 @@ class TelegramAlerter:
             self.send_image(self.latest_image)
 
     def send_image(self, filepath):
+        chat_id = msg['chat']['id']
         self.bot.sendPhoto(chat_id, open('/home/pi/Desktop/Projects/SIOT_Project/output_images/'+filepath+'.png', 'rb'))
 
 if __name__ == '__main__':
