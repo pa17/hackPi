67b0ef530b80bb2a336e9317bb4ce092efa8ad7a
pa17 <paolofalco@gmx.ch>
2019-01-09 13:06:30 +0000

    Revised TelegramAlerter, now fully working!

--- TelegramAlerter.py
+++ TelegramAlerter.py
@@ -15,37 +15,48 @@ class TelegramAlerter:
 
         print("I am listening...")
 
-        self.image_count = 0
-        self.latest_image = ''
-
-        while 1:
-            time.sleep(1000)
+        self.available_images = sorted(os.listdir('/home/pi/Desktop/Projects/SIOT_Project/output_images/'))
+        self.image_count = len(self.available_images)
+        self.latest_image = self.available_images[-1]
+                
+        while True:
+            self.check_images()
+            time.sleep(1)
 
     def handle(self, msg):
-
-        self.check_images()
 
         self.chat_id = msg['chat']['id']
         self.command = msg['text']
 
         print("Got command: %s" % self.command)
 
+        if self.command =='/start':
+            self.bot.sendMessage(self.chat_id, 'Autotracking started!')
+
         if self.command =='/update':
-            self.bot.sendMessage(self.chat_id, 'Update Requested! Last threat detected at: ' + self.latest_image[:-3])
+            self.bot.sendMessage(self.chat_id, 'Update Requested! Last threat detected on the ' + self.latest_image[:10] + ' at ' + self.latest_image[11:-5])
             self.bot.sendPhoto(self.chat_id, open('/home/pi/Desktop/Projects/SIOT_Project/output_images/'+self.latest_image, 'rb'))
+
+        #self.check_images()
 
     def check_images(self):
 
-        available_images = os.listdir('/home/pi/Desktop/Projects/SIOT_Project/output_images/')
+        available_images = sorted(os.listdir('/home/pi/Desktop/Projects/SIOT_Project/output_images/'))
         new_image_count = len(available_images)
+        
+        print("Current Image Count: ", self.image_count, ", New Image Count: ", new_image_count)
 
         if new_image_count > self.image_count:
+            print("New Photo Found: Sending...")
             self.latest_image = available_images[-1]
             self.image_count = new_image_count
-
-            self.bot.sendMessage(self.chat_id, 'Threat Detected! Somebody is at your window at: ' + self.latest_image[:-3])
-            self.bot.sendPhoto(self.chat_id, open('/home/pi/Desktop/Projects/SIOT_Project/output_images/'+self.latest_image, 'rb'))
-
+            print("Sending image: ", self.latest_image)
+            
+            if self.latest_image != '':
+                self.bot.sendMessage(self.chat_id, 'THREAT AUTODETECT! Somebody is at your window on the ' + self.latest_image[:10] + ' at ' + self.latest_image[11:-5])
+                self.bot.sendPhoto(self.chat_id, open('/home/pi/Desktop/Projects/SIOT_Project/output_images/'+self.latest_image, 'rb'))
+                
+                time.sleep(5)
 
 if __name__ == '__main__':
 
